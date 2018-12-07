984c911f3f2ee2a89957fd2819fbd001b7830461
Danilo Cândido <danilod07@gmail.com>
2018-12-07 14:08:43 -0300

    optimize isogram

--- isogram/isogram.rb
+++ isogram/isogram.rb
@@ -1,23 +1,21 @@
 class Isogram
   attr_reader :verifier
+
   def self.isogram?(sentence)
     @verifier = Hash.new(0)
     verify_sentence(sentence)
-    repeated?
   end
 
   class << self
     private
 
     def verify_sentence(sentence)
-      sentence = sentence.gsub(/[^a-zA-Z]/, '')
-      sentence.downcase.chars.each { |letter| @verifier[letter] += 1 }
-      @verifier
-    end
-
-    def repeated?
-      count = @verifier.values.select { |n| n > 1 }.count
-      count.zero? ? true : false
+      sentence = sentence.gsub(/[^a-zA-Z]/, '').downcase
+      sentence.chars.each do |letter|
+        @verifier[letter] += 1
+        return false && break if @verifier[letter] > 1
+      end
+      true
     end
   end
 end

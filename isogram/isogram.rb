4270909988ff06878f6062f5dffd6f7632a4f07a
Danilo Cândido <danilod07@gmail.com>
2018-12-10 15:08:26 -0300

    improve isogram with uniq

--- isogram/isogram.rb
+++ isogram/isogram.rb
@@ -7,13 +7,8 @@ class Isogram
     private
 
     def valid_sentence?(sentence)
-      verifier = Hash.new(0)
       sentence = sentence.scan(/\w+/).join.downcase
-      sentence.chars.each do |letter|
-        verifier[letter] += 1
-        return false if verifier[letter] > 1
-      end
-      true
+      sentence.chars == sentence.chars.uniq
     end
   end
 end

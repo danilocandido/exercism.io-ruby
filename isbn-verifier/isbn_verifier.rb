66d032781dcb5a4ff5b0eac93e394ae6742adea2
Danilo Cândido <danilod07@gmail.com>
2018-12-10 16:05:25 -0300

    resolve isbn verifier improved

--- isbn-verifier/isbn_verifier.rb
+++ isbn-verifier/isbn_verifier.rb
@@ -12,16 +12,20 @@ class IsbnVerifier
     private
 
     def verified?
-      return false if @numbers.size != 9
+      return false if numbers.size != 9
   
       x10 = digit
-      x1, x2, x3, x4, x5, x6, x7, x8, x9 = @numbers.map(&:to_i)
+      x1, x2, x3, x4, x5, x6, x7, x8, x9 = numbers
   
       (x1 * 10 + x2 * 9 + x3 * 8 + x4 * 7 + x5 * 6 + x6 * 5 + x7 * 4 + x8 * 3 + x9 * 2 + x10 * 1) % 11 == 0
     end
 
     def digit
       @digit == 'X' ? 10 : @digit.to_i
+    end
+
+    def numbers
+      @numbers.map(&:to_i)
     end
   end
 end

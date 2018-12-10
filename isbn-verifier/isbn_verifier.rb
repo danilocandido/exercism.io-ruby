c8f68849e8f6b4556ab9be1655ec403e9d506181
Danilo Cândido <danilod07@gmail.com>
2018-12-10 16:04:19 -0300

    resolve isbn verifier

---
+++ isbn-verifier/isbn_verifier.rb
@@ -0,0 +1,27 @@
+class IsbnVerifier
+  def self.valid?(string)
+    return false if string&.empty?
+
+    @digit = string[-1]
+    @numbers = string[0..-2].scan(/\d/)
+
+    verified?
+  end
+
+  class << self
+    private
+
+    def verified?
+      return false if @numbers.size != 9
+  
+      x10 = digit
+      x1, x2, x3, x4, x5, x6, x7, x8, x9 = @numbers.map(&:to_i)
+  
+      (x1 * 10 + x2 * 9 + x3 * 8 + x4 * 7 + x5 * 6 + x6 * 5 + x7 * 4 + x8 * 3 + x9 * 2 + x10 * 1) % 11 == 0
+    end
+
+    def digit
+      @digit == 'X' ? 10 : @digit.to_i
+    end
+  end
+end

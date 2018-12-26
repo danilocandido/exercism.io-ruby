81bb892348ec45f260023c423abb60df1d24dbcf
Danilo Cândido <danilod07@gmail.com>
2018-12-25 21:58:20 -0300

    resolve space age

---
+++ space-age/space_age.rb
@@ -0,0 +1,37 @@
+class SpaceAge
+  def initialize(age_in_seconds)
+    @age_in_seconds = age_in_seconds
+  end
+
+  def on_earth
+    (@age_in_seconds / 31_557_600.00).round 2
+  end
+
+  def on_mercury
+    (on_earth / 0.2408467).round 2
+  end
+
+  def on_venus
+    (on_earth / 0.61519726).round 2
+  end
+
+  def on_mars
+    (on_earth / 1.8808158).round 2
+  end
+
+  def on_jupiter
+    (on_earth / 11.862615).round 2
+  end
+
+  def on_saturn
+    (on_earth / 29.447498).round 2
+  end
+
+  def on_uranus
+    (on_earth / 84.016846).round 2
+  end
+
+  def on_neptune
+    (on_earth / 164.79132).round 2
+  end
+end
